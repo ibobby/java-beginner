0de86b04852155fab653fa1e26ff7d54adb05314
bobby <ibobby0386@gmail.com>
2014-01-31 13:00:42 +0300

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

---
+++ src/Applets/SimpleApplet.java
@@ -0,0 +1,11 @@
+package Applets;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class SimpleApplet extends Applet{
+
+    public void paint(Graphics g) {
+        g.drawString("simple applet", 20, 20);
+    }
+}
