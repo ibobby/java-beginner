a6defb35a41c04b810e4a505ee06b2fba0d7665d
ibobby0386@gmail.com <grasslandforyou06>
2014-07-22 13:54:40 +0400

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

--- src/Threads/ThreadPriority/Golovach/ThreadPrinter.java
+++ src/Threads/ThreadPriority/Golovach/ThreadPrinter.java
@@ -3,5 +3,27 @@ package Threads.ThreadPriority.Golovach;
 /**
  * Created by bobby on 22.07.2014.
  */
-public class ThreadPrinter {
+public class ThreadPrinter implements Runnable {
+
+    private String message;
+    private long sleepTime;
+
+    public ThreadPrinter(String message, long sleepTime) {
+        this.message = message;
+        this.sleepTime = sleepTime;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
+            }
+
+            System.out.println(message);
+        }
+    }
 }
