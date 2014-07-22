83ea92900d9eb75402c76f77c5b8db256c5b36d1
ibobby0386@gmail.com <grasslandforyou06>
2014-07-22 22:51:42 +0400

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

--- src/IllegalNameException.java
+++
@@ -1,9 +0,0 @@
-
-public class IllegalNameException extends Exception{
-
-    IllegalNameException(String name) {
-        super(name);
-        System.out.println("Person name is incorrect");
-    }
-
-}
--- src/InterfaceForm.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-/**
- * Created by Администратор on 06.12.13.
- */
-public class InterfaceForm {
-    private JPasswordField passwordField1;
-}
