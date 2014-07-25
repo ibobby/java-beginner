170eb33e78dc20823ac0221770a0ee95c225fe2e
ibobby0386@gmail.com <grasslandforyou06>
2014-07-25 15:57:24 +0400

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

--- src/Threads/ThreadPriority/Golovach/ConsumerTimed.java
+++ src/Threads/ThreadPriority/Golovach/ConsumerTimed.java
@@ -3,5 +3,24 @@ package Threads.ThreadPriority.Golovach;
 /**
  * Created by bobby on 25.07.2014.
  */
-public class ConsumerTimed {
+public class ConsumerTimed implements Runnable {
+
+    private final SingleElementBufferTimed buffer;
+    private final long timeout;
+
+    public ConsumerTimed(SingleElementBufferTimed buffer, long timeout) {
+        this.buffer = buffer;
+        this.timeout = timeout;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                buffer.get(timeout);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " stopped");
+            }
+        }
+    }
 }
