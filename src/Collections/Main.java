5ebad5ced82633349d9d00efd3138218559003c8
ibobby <ibobby0386@gmail.com>
2014-08-04 22:55:50 +0400

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -11,30 +11,52 @@ public class Main {
     public static void main(String[] args) {
 
         //ArrayList vs LinkedList
+        //AL - good reading
+        //LL must be more stable on add operation
+
+        int arraySize = 100000;
 
         ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("one");
-        arrayList.add("two");
-        arrayList.add("three");
-        System.out.println("ArrayList size = " + arrayList.size());
+
+
+        System.out.println("size = " + arrayList.size());
+
+        //add
+
         long t0 = System.nanoTime();
-        arrayList.remove(2);
+        for (int i = 0; i < arraySize; i++) {
+            arrayList.add("" + i);
+        }
         long t1 = System.nanoTime();
         long dif = t1 - t0;
-        System.out.println("ArrayList del time :" + dif);
+        System.out.println("ArrayList  add time :" + dif);
 
         LinkedList linkedList = new LinkedList();
-        linkedList.add(1);
-        linkedList.add(2);
-        linkedList.add(1);
-        System.out.println(linkedList);
         t0 = System.nanoTime();
-        linkedList.remove(1);
+        for (int i = 0; i < arraySize; i++) {
+            linkedList.add("" + i);
+        }
         t1 = System.nanoTime();
         dif = t1 - t0;
-        System.out.println("LinkedList del time :" + dif);
+        System.out.println("LinkedList add time :" + dif);
 
+        //del
 
+        t0 = System.nanoTime();
+        for (int i = 0; i < arraySize; i++) {
+            arrayList.get(i);
+        }
+        t1 = System.nanoTime();
+        dif = t1 - t0;
+        System.out.println("ArrayList  get time :" + dif);
+
+        t0 = System.nanoTime();
+        for (int i = 0; i < arraySize; i++) {
+            linkedList.get(i);
+        }
+        t1 = System.nanoTime();
+        dif = t1 - t0;
+        System.out.println("LinkedList get time :" + dif);
 
 
     }
