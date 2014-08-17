eff4819073f751bf491fd775cc2a8d6ecee2342b
ibobby <ibobby0386@gmail.com>
2014-08-17 13:04:13 +0400

    Merge branch 'master' of https://github.com/ibobby/java-beginner
    Conflicts:
    	src/VariablesVisibility.java

--- src/Collections/MainTest.java
+++ src/Collections/MainTest.java
@@ -3,6 +3,7 @@ package Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by WhiteMountiens on 13.08.2014.
@@ -10,6 +11,8 @@ import java.util.Map;
 public class MainTest {
 
     public static void main(String[] args) {
+
+//        Sets
 
         HashSet<ClassPoint> pointHashSet = new HashSet();
         pointHashSet.add(new ClassPoint(1, 1));
@@ -25,11 +28,19 @@ public class MainTest {
 
         System.out.println(stringHashSet);
 
+//        Maps
+
         Map stringHashMap = new HashMap<>();
         stringHashMap.put(1, "one");
         stringHashMap.put(1, "two");
 
         System.out.println(stringHashMap);
+
+        Map stringMap = new TreeMap<>();
+        stringMap.put(1, "one");
+        stringMap.put(1, "two");
+
+        System.out.println(stringMap);
 
     }
 
